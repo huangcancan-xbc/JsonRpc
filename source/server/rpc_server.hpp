175ea3cd654195765e08fb47ef66d301b1af7163
小米里的大麦 <hc1960074081@qq.com>
2025-11-17 12:34:02 +0800

    rpc服务端的实现

---
+++ source/server/rpc_server.hpp
@@ -0,0 +1,101 @@
+#pragma once
+#include "../common/dispatcher.hpp"
+#include "../client/rpc_client.hpp"
+#include "rpc_router.hpp"
+#include "rpc_registry.hpp"
+
+
+namespace rpc
+{
+    namespace server
+    {
+        // 注册中心服务端：只需要针对服务注册与发现请求进行处理
+        class RegistryServer
+        {
+        public:
+            using ptr = std::shared_ptr<RegistryServer>;
+
+            RegistryServer(int port)
+                :_pd_manager(std::make_shared<PDManager>()),
+                _dispatcher(std::make_shared<rpc::Dispatcher>())
+            {
+                auto service_cb = std::bind(&PDManager::onServiceRequest, _pd_manager.get(), std::placeholders::_1, std::placeholders::_2);
+                _dispatcher->registerHandler<ServiceRequest>(MType::REQ_SERVICE, service_cb);
+
+                _server = rpc::ServerFactory::create(port);
+                auto message_cb = std::bind(&rpc::Dispatcher::onMessage, _dispatcher.get(), std::placeholders::_1, std::placeholders::_2);
+                _server->setMessageCallback(message_cb);
+
+                auto close_cb = std::bind(&RegistryServer::onConnShutdown, this, std::placeholders::_1);
+                _server->setCloseCallback(close_cb);
+            }
+
+            void start()
+            {
+                _server->start();
+            }
+
+        private:
+            void onConnShutdown(const BaseConnection::ptr &conn)
+            {
+                _pd_manager->onConnShutdown(conn);
+            }
+
+        private:
+            PDManager::ptr _pd_manager;
+            Dispatcher::ptr _dispatcher;
+            BaseServer::ptr _server;
+        };
+
+
+
+        class RpcServer
+        {
+            using ptr = std::shared_ptr<RpcServer>;
+
+            RpcServer(const Address &access_addr,
+                      bool enableRegistry = false,
+                      const Address &registry_server_addr = Address())
+                : _enableRegistry(enableRegistry),
+                  _access_addr(access_addr),
+                  _router(std::make_shared<rpc::server::RpcRouter>()),
+                  _dispatcher(std::make_shared<rpc::Dispatcher>())
+            {
+                if(enableRegistry)
+                {
+                    _reg_client = std::make_shared<client::RegistryClient>(registry_server_addr.first, registry_server_addr.second);
+                }
+
+                auto rpc_cb = std::bind(&rpc::server::RpcRouter::onRpcRequest, _router.get(), std::placeholders::_1, std::placeholders::_2);
+                _dispatcher->registerHandler<rpc::RpcRequest>(rpc::MType::REQ_RPC, rpc_cb);
+
+                _server=rpc::ServerFactory::create(access_addr.second);
+                auto message_cb = std::bind(&rpc::Dispatcher::onMessage, _dispatcher.get(), std::placeholders::_1, std::placeholders::_2);
+                _server->setMessageCallback(message_cb);
+            }
+
+            void registerMethod(const ServiceDescribe::ptr &service)
+            {
+                if(_enableRegistry)
+                {
+                    _reg_client->registryMethod(service->method(), _access_addr);
+                }
+
+                _router->registerMethod(service);
+            }
+
+            void start()
+            {
+                _server->start();
+            }
+
+        private:
+            bool _enableRegistry;
+            Address _access_addr;
+            client::RegistryClient::ptr _reg_client;
+            RpcRouter::ptr _router;
+            Dispatcher::ptr _dispatcher;
+            BaseServer::ptr _server;
+        };
+    }
+}
